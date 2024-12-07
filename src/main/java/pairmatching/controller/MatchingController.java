b543078c53c84e1fbed9b16819dc702fe06fa00e
woojincheon <qpqpqp0102@naver.com>
2024-12-07 16:00:40 +0900

    feat: MatchingController 작성

--- src/main/java/pairmatching/controller/MatchingController.java
+++ src/main/java/pairmatching/controller/MatchingController.java
@@ -1,7 +1,28 @@
 package pairmatching.controller;
 
-public class MatchingController {
-    public void run() {
+import pairmatching.model.Crews;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
 
+public class MatchingController {
+    private Crews crews;
+
+    public void run() {
+        OutputView.printSelectFunctionMessage();
+        String selection = InputView.selectFunction();
+        // TODO validation
+
+        if(selection.equals("1")) {
+
+        }
+    }
+
+    private void matchPair() {
+        String[] mission = InputView.selectMission().split(", ");
+
+    }
+
+    public MatchingController(Crews crews) {
+        this.crews = crews;
     }
 }
