754cca5852c7692c84caaaf7b88b43d14380b878
woojincheon <qpqpqp0102@naver.com>
2024-12-07 15:57:54 +0900

    feat: 출력문, 입력받는 메서드 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,5 +1,7 @@
 package pairmatching.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,5 +12,17 @@ public class InputView{
     public static List<String> readFile(String fileName) throws IOException {
         Path path = Paths.get(fileName);
         return Files.readAllLines(path);
+    }
+
+    public static String selectFunction() {
+        return input();
+    }
+
+    public static String selectMission() {
+        return input();
+    }
+
+    private static String input() {
+        return Console.readLine();
     }
 }
